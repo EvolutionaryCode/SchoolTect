1e053b0d751971a417126ec35818dab7d1c6a18c
Austin Grandpre <ausgran@icloud.com>
2018-10-31 13:19:21 -0400

    Got A Testing Version Working

--- Chrome_Extension/main.js
+++ Chrome_Extension/main.js
@@ -1,7 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-console.log("admin");
+class Main {
+    constructor(msg) {
+        console.log(typeof msg, msg)
+        if (msg) {
+            this.pasteTrigger();
+        }
+        pasteTrigger()
+        {
+            document.addEventListener('paste', "copy and paste is disabled", true);
+        }
+        chrome.runtime.onMessage.addListener((response) => {
+            new CopyPastePro(response.isEnabled);
+        })
 
+
+        if (performance.navigation.type === 1) {
+            chrome.runtime.sendMessage({
+                refresh: "yes"
+            });
+        }
+    }
+}
